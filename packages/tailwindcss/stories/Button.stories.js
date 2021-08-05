faa12a41fce9cd6c537f904c6fda1e7fd3598602
Kimiaki Kuno <knokmki612@gmail.com>
2021-08-05 12:09:37 +0900

    feat: tailwindcss pluginのスタイルシートでStoryが書けるように

--- packages/tailwindcss/stories/Button.stories.js
+++ packages/tailwindcss/stories/Button.stories.js
@@ -1,43 +1,25 @@
-import { createButton } from './Button';
+import clsx from "clsx";
 
 export default {
-  title: 'Example/Button',
+  title: "Button",
   argTypes: {
-    label: { control: 'text' },
-    primary: { control: 'boolean' },
-    backgroundColor: { control: 'color' },
-    size: {
-      control: { type: 'select', options: ['small', 'medium', 'large'] },
-    },
-    onClick: { action: 'onClick' },
+    label: {control: 'text'},
+    text: { control: 'boolean' },
+    rounded: { control: 'boolean' },
+    stretched: { control: 'boolean' },
   },
 };
 
-const Template = ({ label, ...args }) => {
-  // You can either use a function to create DOM elements or use a plain html string!
-  // return `<div>${label}</div>`;
-  return createButton({ label, ...args });
+const Template = ({ label, text, rounded, stretched, ...args }) => {
+  return `<button type="button" class="${clsx(
+    "btn",
+    { "is-text": text },
+    { "is-rounded": rounded },
+    { "is-stretched": stretched },
+  )}">${label}</button>`;
 };
 
-export const Primary = Template.bind({});
-Primary.args = {
-  primary: true,
-  label: 'Button',
-};
-
-export const Secondary = Template.bind({});
-Secondary.args = {
-  label: 'Button',
-};
-
-export const Large = Template.bind({});
-Large.args = {
-  size: 'large',
-  label: 'Button',
-};
-
-export const Small = Template.bind({});
-Small.args = {
-  size: 'small',
-  label: 'Button',
+export const Default = Template.bind({});
+Default.args = {
+  label: "Button",
 };
