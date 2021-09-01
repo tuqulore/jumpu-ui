f719b84fd2e5e0617c568c1e47c86f0d1bf2f4ec
Hidetaro7 <hidetaro7@gmail.com>
2021-09-01 17:28:57 +0900

    feat: ShadowedCardが不要と判断したので削除

--- packages/tailwindcss/card.js
+++ packages/tailwindcss/card.js
@@ -1,24 +1,19 @@
 const plugin = require("tailwindcss/plugin");
 const theme = require("./theme");
-const cardStyle = require("./lib/cardStyle");
 
 module.exports = plugin.withOptions(
     // NOTE: couldn't merge plugin-side theme when wrote the plugin without withOptions
     () => {
         return ({ addComponents, theme }) => {
-            const { base } = cardStyle(theme);
             addComponents([
                 {
                     ".card": {
-                        ...base,
+                        backgroundColor: theme("colors.white"),
+                        borderRadius: theme("borderRadius.md"),
+                        overflow: "hidden",
+                        "border-width": "1px",
                         "border-color": theme("colors.gray.200"),
-                    },
-
-                    ".shadowed-card": {
-                        ...base,
-                        "border-color": theme("colors.gray.100"),
-                        "box-shadow": `0 4px 6px -1px rgba(0, 0, 0, 0.1), 0 2px 4px -1px rgba(0, 0, 0, 0.06)`,
-                    },
+                    }
                 },
             ]);
         };
