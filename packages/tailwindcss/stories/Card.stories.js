f1333a9a20e1a323586adcd787d5ce4b38446824
Hidetaro7 <hidetaro7@gmail.com>
2021-09-01 17:37:41 +0900

    feat: Cardコンポーネントの出来上がり

--- packages/tailwindcss/stories/Card.stories.js
+++ packages/tailwindcss/stories/Card.stories.js
@@ -7,10 +7,23 @@ export default {
 };
 
 const Template = ({ label }) => {
-    return `<div class="card">${label}</div>`;
+    return `
+        <div class="card px-4 py-3 mb-4">${label}</div> 
+    `;
 };
 
 export const Default = Template.bind({});
 Default.args = {
+    label: "カード",
+};
+
+const ShadowedTemplate = ({ label }) => {
+    return `
+        <div class="card px-4 py-3 border-0 shadow-lg">${label}</div>    
+    `;
+};
+
+export const Shadowed = ShadowedTemplate.bind({});
+Shadowed.args = {
     label: "カード",
 };
