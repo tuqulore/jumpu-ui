67e75d881a2b33402252390e48a3d6143bd55b2a
Hidetaro7 <hidetaro7@gmail.com>
2021-09-02 12:57:35 +0900

    delete: typographyはTailwind Typographyを使うので独自適宜は不要

--- packages/tailwindcss/index.js
+++ packages/tailwindcss/index.js
@@ -1,8 +1,7 @@
-const button = require("./button");
-const outlinedButton = require("./outlinedButton");
-const status = require("./status");
-const card = require("./card");
-const form = require("./form");
-const typography = require("./typography");
+const button = require('./button');
+const outlinedButton = require('./outlinedButton');
+const status = require('./status');
+const card = require('./card');
+const form = require('./form');
 
-module.exports = [button, outlinedButton, status, card, form, typography];
+module.exports = [button, outlinedButton, status, card, form];
