a63ce82fa6e0c54196fb2b4b6b280be481cd2c9c
Hidetaro7 <hidetaro7@gmail.com>
2021-09-06 21:44:35 +0900

    refactor: コメント消し忘れ

--- packages/tailwindcss/iconbutton.js
+++ packages/tailwindcss/iconbutton.js
@@ -58,29 +58,3 @@ module.exports = plugin.withOptions(
   },
   () => ({ theme })
 );
-
-/* @mixin icon-button {
-  @apply inline-flex justify-center items-center  text-base text-gray-800 bg-gray-100 rounded-full outline-none w-8 h-8 transition bg-opacity-50 relative dark:bg-gray-800 dark:text-white;
-
-  .icon-button-inner {
-    @apply w-full h-full rounded-full inline-flex justify-center items-center;
-  }
-
-  &:focus {
-    @apply outline-none;
-  }
-
-  &:hover {
-    @apply bg-gray-50 dark:bg-gray-700;
-
-    &::after {
-      content: attr(data-label);
-
-      @apply inline-flex left-1/2 -top-10 px-4 py-1 absolute transform -translate-x-1/2 bg-gray-100 rounded text-sm whitespace-nowrap dark:bg-gray-800 dark:text-white bg-opacity-50;
-    }
-  }
-
-  &:active {
-    @apply bg-gray-200 dark:bg-gray-800;
-  }
-} */
