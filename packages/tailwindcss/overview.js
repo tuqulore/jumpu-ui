930fbd8f86b52ffec893cca584ad195397105f7d
Hidetaro7 <hidetaro7@gmail.com>
2021-09-09 18:01:49 +0900

    feat: Overviewを作成

---
+++ packages/tailwindcss/overview.js
@@ -0,0 +1,15 @@
+const plugin = require("tailwindcss/plugin");
+const theme = require("./theme");
+
+module.exports = plugin.withOptions(
+  () => {
+    return ({ addComponents, theme }) => {
+      addComponents([
+        {
+          ".overview": {},
+        },
+      ]);
+    };
+  },
+  () => ({ theme })
+);
