312157b8e894e5e7e1ddd3fc696e1c8024171403
Hidetaro7 <hidetaro7@gmail.com>
2021-09-09 19:12:16 +0900

    feat: フォントサイズにxxs（10px）を追加

--- packages/tailwindcss/tailwind.config.js
+++ packages/tailwindcss/tailwind.config.js
@@ -2,7 +2,11 @@ module.exports = {
   purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      fontSize: {
+        xxs: ["10px", "1.6em"],
+      },
+    },
   },
   variants: {
     extend: {},
