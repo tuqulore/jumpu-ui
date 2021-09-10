43ab146280a756a4bbc1d1301ec605beca80aa7d
knokmki612 <knokmki612@users.noreply.github.com>
2021-09-10 01:52:43 +0000

    bot: format

--- packages/tailwindcss/stories/Button.stories.js
+++ packages/tailwindcss/stories/Button.stories.js
@@ -3,10 +3,10 @@ import clsx from "clsx";
 export default {
   title: "Button",
   argTypes: {
-    label: {control: 'text'},
-    text: { control: 'boolean' },
-    rounded: { control: 'boolean' },
-    stretched: { control: 'boolean' },
+    label: { control: "text" },
+    text: { control: "boolean" },
+    rounded: { control: "boolean" },
+    stretched: { control: "boolean" },
   },
 };
 
@@ -15,7 +15,7 @@ const Template = ({ label, text, rounded, stretched, ...args }) => {
     "btn",
     { "is-text": text },
     { "is-rounded": rounded },
-    { "is-stretched": stretched },
+    { "is-stretched": stretched }
   )}">${label}</button>`;
 };
 
