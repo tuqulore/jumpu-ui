a59cd52d758e3cf1162419d40f970c336695b2d6
Hideki Akiba <hidetaro7@gmail.com>
2021-09-14 12:35:15 +0900

    refactor: aria-controlsが入っていない
    Co-authored-by: Kimiaki Kuno <knokmki612@gmail.com>

--- packages/tailwindcss/stories/Accordion.stories.js
+++ packages/tailwindcss/stories/Accordion.stories.js
@@ -20,8 +20,8 @@ export const Default = Template.bind({});
 const CollapsedTemplate = () => {
   return `
       <div class="accordion">
-          <button class="accordion-header" aria-expanded="false">人間失格</button>
-          <div class="accordion-body" aria-hidden="true">
+          <button class="accordion-header" aria-expanded="false" aria-controls="accordion-body">人間失格</button>
+          <div id="accordion-body" class="accordion-body" aria-hidden="true">
               <div class="p-4">
               <p>恥の多い生涯を送って来ました。自分には、人間の生活というものが、見当つかないのです。自分は東北の田舎に生れましたので、汽車をはじめて見たのは、よほど大きくなってからでした。自分は停車場のブリッジを、上って、降りて、そうしてそれが線路をまたぎ越えるために造られたものだという事には全然気づかず、ただそれは停車場の構内を外国の遊戯場みたいに、複雑に楽しく、ハイカラにするためにのみ、設備せられてあるものだとばかり思っていました。</p>
               <p>しかも、かなり永い間そう思っていたのです。ブリッジの上ったり降りたりは、自分にはむしろ、ずいぶん垢抜けのした遊戯で、それは鉄道のサーヴィスの中でも、最も気のきいたサーヴィスの一つだと思っていたのですが、のちにそれはただ旅客が線路をまたぎ越えるための頗る実利的な階段に過ぎないのを発見して、にわかに興が覚めました。</p>
