30a7f3b7bb1559f34ecd49b3de595e3960e50c1b
Hideki Akiba <hidetaro7@gmail.com>
2021-09-17 16:50:26 +0900

    refactor: 説明の必要がないaria-labelがある
    Co-authored-by: Kimiaki Kuno <knokmki612@gmail.com>

--- packages/tailwindcss/accordion.js
+++ packages/tailwindcss/accordion.js
@@ -25,7 +25,7 @@ module.exports = plugin.withOptions(
                 height: `${theme("height.4")}`,
                 marginRight: theme("spacing.2"),
                 "background-image": `url("${svgToDataUri(
-                  `<svg aria-label="icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path d="M443.5 162.6l-7.1-7.1c-4.7-4.7-12.3-4.7-17 0L224 351 28.5 155.5c-4.7-4.7-12.3-4.7-17 0l-7.1 7.1c-4.7 4.7-4.7 12.3 0 17l211 211.1c4.7 4.7 12.3 4.7 17 0l211-211.1c4.8-4.7 4.8-12.3.1-17z"></path></svg>`
+                  `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path d="M443.5 162.6l-7.1-7.1c-4.7-4.7-12.3-4.7-17 0L224 351 28.5 155.5c-4.7-4.7-12.3-4.7-17 0l-7.1 7.1c-4.7 4.7-4.7 12.3 0 17l211 211.1c4.7 4.7 12.3 4.7 17 0l211-211.1c4.8-4.7 4.8-12.3.1-17z"></path></svg>`
                 )}")`,
                 "background-position": `center`,
                 "background-repeat": `no-repeat`,
