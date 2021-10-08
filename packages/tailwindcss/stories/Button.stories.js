65c5a7d6a08ad4a21f466d1bea38c9d09c2bfab0
Hidetaro7 <hidetaro7@gmail.com>
2021-10-08 09:49:34 +0900

    feat: ButtonのRingバリアントではなくOthersとして使用例にした

--- packages/tailwindcss/stories/Button.stories.js
+++ packages/tailwindcss/stories/Button.stories.js
@@ -86,56 +86,6 @@ const ColoredTemplate = () => {
 
 export const Colored = ColoredTemplate.bind({});
 
-const RingColoredTemplate = () => {
-  return `
-    <p class="mb-4">
-      <button type="button" class="button ring-opacity-50 ring-4">ボタン</button>
-    </p>
-    <p class="mb-4">
-      <button type="button" class="button bg-success ring-success ring-opacity-50 ring-4">成功しました</button>
-    </p>
-    <p class="mb-4">
-      <button type="button" class="button bg-danger ring-danger ring-opacity-50 ring-4">削除します</button>
-    </p>
-    <p class="mb-4">
-      <button type="button" class="button bg-warning text-black ring-warning ring-opacity-50 ring-4">下書き保存</button>
-    </p>
-    <p class="mb-4">
-      <button type="button" class="button bg-gray-100 text-black ring-gray-200 ring-opacity-60 ring-4">白いボタン</button>
-    </p>
-    <p>
-      <button type="button" class="button bg-gray-700 ring-gray-700 ring-opacity-50 ring-4">黒いボタン</button>
-    </p>
-  `;
-};
-
-export const RingColored = RingColoredTemplate.bind({});
-
-const OutlinedRingColorTemplate = () => {
-  return `
-    <p class="mb-4">
-      <button type="button" class="button ring-opacity-50 ring-offset-1 ring-4">ボタン</button>
-    </p>
-    <p class="mb-4">
-      <button type="button" class="button bg-success ring-success ring-opacity-50 ring-offset-1 ring-4">成功しました</button>
-    </p>
-    <p class="mb-4">
-      <button type="button" class="button bg-danger ring-danger ring-opacity-50 ring-offset-1 ring-4">削除します</button>
-    </p>
-    <p class="mb-4">
-      <button type="button" class="button bg-warning text-black ring-warning ring-opacity-50 ring-offset-1 ring-4">下書き保存</button>
-    </p>
-    <p class="mb-4">
-      <button type="button" class="button bg-gray-100 text-black ring-gray-200 ring-opacity-60 ring-offset-1 ring-4">白いボタン</button>
-    </p>
-    <p>
-      <button type="button" class="button bg-gray-700 ring-gray-700 ring-opacity-50 ring-offset-1 ring-4">黒いボタン</button>
-    </p>
-  `;
-};
-
-export const OutlinedRingColor = OutlinedRingColorTemplate.bind({});
-
 const DisabledTemplate = () => {
   return `
     <button type="button" disabled class="button">利用できないボタン</button>
@@ -143,3 +93,31 @@ const DisabledTemplate = () => {
 };
 
 export const Disabled = DisabledTemplate.bind({});
+
+const OtherTemplate = () => {
+  return `
+    <h4>focus時、Tailwind CSSのRingユーティリティを使用した例</h4>
+    <p>マウスやタッチなどでfocus状態にする</p>
+    <div class="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-6 gap-4 py-4 mb-8">
+      <button type="button" class="button ring-opacity-50 focus:ring-4">ボタン</button>
+      <button type="button" class="button bg-success ring-success ring-opacity-50 focus:ring-4">成功</button>
+      <button type="button" class="button bg-danger ring-danger ring-opacity-50 focus:ring-4">削除</button>
+      <button type="button" class="button bg-warning text-black ring-warning ring-opacity-50 focus:ring-4">下書き保存</button>
+      <button type="button" class="button bg-gray-100 text-black ring-gray-200 ring-opacity-60 focus:ring-4">ボタン</button>
+      <button type="button" class="button bg-gray-700 ring-gray-700 ring-opacity-50 focus:ring-4">ボタン</button>
+    </div>
+
+    <h4>focus時、Tailwind CSSのRing Offsetユーティリティを使用した例</h4>
+    <p>マウスやタッチなどでfocus状態にする</p>
+    <div class="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-6 gap-4 py-4">
+      <button type="button" class="button ring-opacity-50 ring-offset-1 focus:ring-4">ボタン</button>
+      <button type="button" class="button bg-success ring-success ring-opacity-50 ring-offset-1 focus:ring-4">成功しました</button>
+      <button type="button" class="button bg-danger ring-danger ring-opacity-50 ring-offset-1 focus:ring-4">削除します</button>
+      <button type="button" class="button bg-warning text-black ring-warning ring-opacity-50 ring-offset-1 focus:ring-4">下書き保存</button>
+      <button type="button" class="button bg-gray-100 text-black ring-gray-200 ring-opacity-60 ring-offset-1 focus:ring-4">ボタン</button>
+      <button type="button" class="button bg-gray-700 ring-gray-700 ring-opacity-50 ring-offset-1 focus:ring-4">ボタン</button>
+    </div>
+  `;
+};
+
+export const Others = OtherTemplate.bind({});
