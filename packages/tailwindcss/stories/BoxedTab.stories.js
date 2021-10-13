4c68051ce53d197c623e276500e9aa98670df4bd
Hidetaro7 <hidetaro7@gmail.com>
2021-10-13 18:18:31 +0900

    feat: BoxedTabにaタグを追加

--- packages/tailwindcss/stories/BoxedTab.stories.js
+++ packages/tailwindcss/stories/BoxedTab.stories.js
@@ -16,6 +16,20 @@ const Template = () => {
 };
 export const Default = Template.bind({});
 
+const AnchorTemplate = () => {
+  return `
+  <div class="boxed-tabs">
+    <div role="tablist" aria-label="Sample BoxedTabs">
+      <a href="#" role="tab" aria-selected="false" aria-controls="panel-1" id="tab-1" tabindex="0">ホーム</a>
+      <a href="#" role="tab" aria-selected="false" aria-controls="panel-2" id="tab-2" tabindex="-1">タスク</a>
+      <a href="#" role="tab" aria-selected="true" aria-controls="panel-3" id="tab-3" tabindex="-1">実績</a>
+      <a href="#" role="tab" aria-selected="false" aria-controls="panel-4" id="tab-4" tabindex="-1">会話</a>
+    </div>
+  </div>
+    `;
+};
+export const Anchor = AnchorTemplate.bind({});
+
 const SmallTemplate = () => {
   return `
   <div class="boxed-tabs">
