c8ffccd686547d919252a198165ba9549bcba32a
Hidetaro7 <hidetaro7@gmail.com>
2021-10-15 14:24:49 +0900

    feat: OGPシェアしたときの見せ方

--- packages/tailwindcss/stories/Chat.stories.js
+++ packages/tailwindcss/stories/Chat.stories.js
@@ -3,6 +3,8 @@ export default {
 };
 
 const avatarImage = require("./assets/avatar_photo.jpg");
+const posterImage = require("./assets/Cover.png");
+const faviconImage = require("./assets/sample_favicon.ico");
 
 const Template = () => {
   return `
@@ -29,3 +31,38 @@ const Template = () => {
 };
 
 export const Default = Template.bind({});
+
+const ShareTemplate = () => {
+  return `
+      <div class="container max-w-2xl">
+          <div class="mb-4 flex">
+              <div class="avatar flex-shrink-0 mr-4">
+                  <a href="#">
+                      <img src="${avatarImage}" alt="城田 亜利沙" />
+                  </a>
+              </div>
+              <div>
+                  <div class="flex items-center mb-2">
+                      <div class="font-bold mr-4"><a href="#">城田 亜利沙</a></div>
+                      <div class="text-xs">9:15</div>
+                  </div>
+                  <div class="flex items-center border border-gray-300 rounded-lg overflow-hidden">
+                    <a href="#" class="w-32 h-32 flex-shrink-0">
+                        <img src="${posterImage}" alt="" class="w-full h-full object-cover" />
+                    </a>
+                    <div class="px-4 py-3">
+                        <a href="#" class="mb-1 font-bold block hover:underline">つくり・つたえるUIデザイン | 株式会社ツクロア</a>
+                            <div class="mb-2 text-sm text-gray-700">人の行動と心を理解しプロジェクトの成長を支援するUIデザイン・アドバイザー</div>
+                            <div class="text-xs text-gray-500 leading-none flex items-center">
+                            <img src="${faviconImage}" class="w-3 h-3 inline mr-1" alt="" />
+                            <a href="#">tuqulore.com</a>
+                        </div>
+                    </div>
+                  </div>
+              </div>
+          </div>
+      </div>
+      `;
+};
+
+export const Share = ShareTemplate.bind({});
