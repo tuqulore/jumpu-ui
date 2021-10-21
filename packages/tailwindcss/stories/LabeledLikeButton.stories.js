df22fa736030c6f608934ed29bcbf7bd86dca96c
Hidetaro7 <hidetaro7@gmail.com>
2021-10-22 08:08:04 +0900

    refactor(LikeButton, LabeledLikeButton): いいねラベルを動詞

--- packages/tailwindcss/stories/LabeledLikeButton.stories.js
+++ packages/tailwindcss/stories/LabeledLikeButton.stories.js
@@ -4,14 +4,14 @@ export default {
 
 const Template = () => {
   return `
-        <button type="button" class="labeled-like-button" aria-pressed="false" aria-label="いいね">いいね</button>
+        <button type="button" class="labeled-like-button" aria-pressed="false" aria-label="いいねする">いいね</button>
     `;
 };
 export const Default = Template.bind({});
 
 const LikedLabeledTemplate = () => {
   return `
-        <button type="button" class="labeled-like-button" aria-pressed="true" aria-label="いいね">いいね</button>
+        <button type="button" class="labeled-like-button" aria-pressed="true" aria-label="いいねを取り消す">いいね</button>
     `;
 };
 export const LikedLabeled = LikedLabeledTemplate.bind({});
--- packages/tailwindcss/stories/LikeButton.stories.js
+++ packages/tailwindcss/stories/LikeButton.stories.js
@@ -4,14 +4,14 @@ export default {
 
 const Template = () => {
   return `
-        <button type="button" class="like-button" aria-pressed="false" aria-label="いいね"></button>
+        <button type="button" class="like-button" aria-pressed="false" aria-label="いいねする"></button>
     `;
 };
 export const Default = Template.bind({});
 
 const LikedTemplate = () => {
   return `
-        <button type="button" class="like-button" aria-pressed="true" aria-label="いいね"></button>
+        <button type="button" class="like-button" aria-pressed="true" aria-label="いいねを取り消す"></button>
     `;
 };
 export const Liked = LikedTemplate.bind({});
