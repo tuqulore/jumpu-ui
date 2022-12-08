0a6c79604d8dbcc57cd2c093d304a04c172d76c5
Hidetaro7 <hidetaro7@gmail.com>
2022-12-08 21:09:37 +0900

    Revert "feat: accordion要素をdetails~summary要素へ"
    This reverts commit 2cac3e68778139bf0669d936935935697e2964db.

--- packages/tailwindcss/accordion.js
+++ packages/tailwindcss/accordion.js
@@ -7,34 +7,45 @@ module.exports = plugin.withOptions(
     return ({ addComponents, theme }) => {
       addComponents({
         [`.${theme("jumpu.prefix")}accordion`]: {
-          details: {
-            summary: {
-              width: "100%",
-              padding: `${theme("spacing.2")} ${theme("spacing.4")}`,
-              paddingLeft: theme("spacing.6"),
-              "list-style": "none",
-              position: "relative",
-              cursor: "pointer",
-              "&::marker": {
-                display: "none",
-              },
+          "& > [aria-expanded]": {
+            width: "100%",
+            padding: `${theme("spacing.3")} ${theme("spacing.4")}`,
+            display: "flex",
+            justifyContent: "flex-start",
+            alignItems: "center",
+            border: `1px solid ${theme("colors.gray.300")}`,
+            cursor: "pointer",
+            "&::before": {
+              content: "''",
+              display: "block",
+              transition: "0.5s",
+              width: `${theme("width.4")}`,
+              height: `${theme("height.4")}`,
+              marginRight: theme("spacing.2"),
+              "background-image": `url("${svgToDataUri(
+                `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path d="M443.5 162.6l-7.1-7.1c-4.7-4.7-12.3-4.7-17 0L224 351 28.5 155.5c-4.7-4.7-12.3-4.7-17 0l-7.1 7.1c-4.7 4.7-4.7 12.3 0 17l211 211.1c4.7 4.7 12.3 4.7 17 0l211-211.1c4.8-4.7 4.8-12.3.1-17z"></path></svg>`
+              )}")`,
+              "background-position": `center`,
+              "background-repeat": `no-repeat`,
+              "background-size": `${theme("spacing.4")}`,
+            },
+            "&:hover": {
+              backgroundColor: `${theme("colors.gray.50")}`,
+            },
+            "&:active": {
+              backgroundColor: `${theme("colors.gray.100")}`,
+            },
+            "&[aria-expanded='false']": {
               "&::before": {
-                content: `url("${svgToDataUri(
-                  `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path d="M443.5 162.6l-7.1-7.1c-4.7-4.7-12.3-4.7-17 0L224 351 28.5 155.5c-4.7-4.7-12.3-4.7-17 0l-7.1 7.1c-4.7 4.7-4.7 12.3 0 17l211 211.1c4.7 4.7 12.3 4.7 17 0l211-211.1c4.8-4.7 4.8-12.3.1-17z"></path></svg>`
-                )}")`,
-                width: `${theme("width.3")}`,
-                display: "block",
-                position: "absolute",
-                left: 0,
-                transform: "rotate(-90deg)",
+                transform: "rotate(180deg)",
               },
             },
-            "&[open]": {
-              summary: {
-                "&::before": {
-                  transform: "rotate(0deg)",
-                },
-              },
+          },
+          "& > [aria-hidden]": {
+            transition: "0.2s",
+            overflow: "hidden",
+            "&[aria-hidden='true']": {
+              display: "none",
             },
           },
         },
