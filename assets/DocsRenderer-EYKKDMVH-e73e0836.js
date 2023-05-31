ece78cb16978567362a3ea8ace9759c2fd0a53a6
knokmki612 <knokmki612@users.noreply.github.com>
2023-05-31 12:01:08 +0000

    deploy: b05ee425c4707a87da83a37c2758719a115d2e50

---
+++ assets/DocsRenderer-EYKKDMVH-e73e0836.js
@@ -0,0 +1,2 @@
+import{D as d,d as n}from"./chunk-PCJTTTQV-b0c5fd12.js";import"./iframe-038e0abb.js";import"../sb-preview/runtime.js";import"./_commonjsHelpers-87174ba5.js";import"./index-d475d2ea.js";import"./index-d37d4223.js";import"./index-f25de5ba.js";import"./index-356e4a49.js";export{d as DocsRenderer,n as defaultComponents};
+//# sourceMappingURL=DocsRenderer-EYKKDMVH-e73e0836.js.map
---
+++ assets/ToggleSwitch.stories-c3b50e46.js
@@ -0,0 +1,37 @@
+import{M as p,C as o,b as n}from"./chunk-PCJTTTQV-b0c5fd12.js";import{h as a}from"./html-ce322a52.js";import{j as s}from"./jsx-runtime-ef60954b.js";import{u as i}from"./index-47ccab8b.js";import"./iframe-038e0abb.js";import"../sb-preview/runtime.js";import"./_commonjsHelpers-87174ba5.js";import"./index-d475d2ea.js";import"./index-d37d4223.js";import"./index-f25de5ba.js";import"./index-356e4a49.js";function r(t){const e=Object.assign({h1:"h1",h2:"h2",h3:"h3"},i(),t.components);return s.jsxs(s.Fragment,{children:[s.jsx(p,{title:"Forms/ToggleSwitch"}),`
+`,s.jsx(e.h1,{id:"toggleswitch",children:"ToggleSwitch"}),`
+`,s.jsx(o,{children:s.jsx(n,{name:"Default",children:a`
+      <div class="jumpu-toggle-switch w-16 h-8">
+        <input id="default" type="checkbox" />
+        <label for="default" />
+        <span class="w-8 h-8"></span>
+      </div>
+    `})}),`
+`,s.jsx(e.h2,{id:"stories",children:"Stories"}),`
+`,s.jsx(e.h3,{id:"small",children:"Small"}),`
+`,s.jsx(o,{children:s.jsx(n,{name:"Small",children:a` <div class="jumpu-toggle-switch w-8 h-4">
+      <input id="small" type="checkbox" />
+      <label for="small" />
+      <span class="w-4 h-4"></span>
+    </div>`})})]})}function h(t={}){const{wrapper:e}=Object.assign({},i(),t.components);return e?s.jsx(e,{...t,children:s.jsx(r,{...t})}):r(t)}const c=()=>a`
+      <div class="jumpu-toggle-switch w-16 h-8">
+        <input id="default" type="checkbox" />
+        <label for="default" />
+        <span class="w-8 h-8"></span>
+      </div>
+    `;c.storyName="Default";c.parameters={storySource:{source:`html\`
+      <div class="jumpu-toggle-switch w-16 h-8">
+        <input id="default" type="checkbox" />
+        <label for="default" />
+        <span class="w-8 h-8"></span>
+      </div>
+    \``}};const m=()=>a` <div class="jumpu-toggle-switch w-8 h-4">
+      <input id="small" type="checkbox" />
+      <label for="small" />
+      <span class="w-4 h-4"></span>
+    </div>`;m.storyName="Small";m.parameters={storySource:{source:`html\` <div class="jumpu-toggle-switch w-8 h-4">
+      <input id="small" type="checkbox" />
+      <label for="small" />
+      <span class="w-4 h-4"></span>
+    </div>\``}};const l={title:"Forms/ToggleSwitch",tags:["stories-mdx"],includeStories:["defaultStory","small"]};l.parameters=l.parameters||{};l.parameters.docs={...l.parameters.docs||{},page:h};const M=["defaultStory","small"];export{M as __namedExportsOrder,l as default,c as defaultStory,m as small};
+//# sourceMappingURL=ToggleSwitch.stories-c3b50e46.js.map
