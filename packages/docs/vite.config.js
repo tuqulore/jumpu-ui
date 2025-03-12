0d1f72ffc58f06b1495133bedc5bce21a0610fd1
Kimiaki Kuno <knokmki612@gmail.com>
2025-03-12 13:40:52 +0900

    Use pnpm

--- packages/docs/vite.config.js
+++ packages/docs/vite.config.js
@@ -1,7 +1,6 @@
-import preact from "@preact/preset-vite";
 import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "vite";
 
 export default defineConfig({
-  plugins: [preact(), tailwindcss()],
+  plugins: [tailwindcss()],
 });
